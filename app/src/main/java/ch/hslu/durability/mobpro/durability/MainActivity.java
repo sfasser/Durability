aadab6e7794bd0bf60e4b3badd0ebda331a037fa
PascalBurri <pascal.burri.01@stud.hslu.ch>
2017-05-09 15:46:41 +0200

    Scanner Implementiert

--- app/src/main/java/ch/hslu/durability/mobpro/durability/MainActivity.java
+++ app/src/main/java/ch/hslu/durability/mobpro/durability/MainActivity.java
@@ -1,15 +1,62 @@
 package ch.hslu.durability.mobpro.durability;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private Button scanBtn;
+    private TextView formatTxt;
+    private TextView contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Bitch
-        //Scheiss GIT
+
+        scanBtn = (Button)findViewById(R.id.button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        //respond to clicks
+
+        if(v.getId()==R.id.button) {
+            //scan
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanningResult != null) {
+            //we have a result
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            formatTxt.setText("FORMAT: " + scanFormat);
+            contentTxt.setText("CONTENT: " + scanContent);
+
+        } else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
     }
 }
