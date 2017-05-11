e0d5d508d068f54491d4dffe5a2692c722c49831
PascalBurri <pascal.burri.01@stud.hslu.ch>
2017-05-11 20:44:10 +0200

    Layout angepasst

--- app/src/main/java/ch/hslu/durability/mobpro/durability/AddActivity.java
+++ app/src/main/java/ch/hslu/durability/mobpro/durability/AddActivity.java
@@ -1,21 +1,34 @@
 package ch.hslu.durability.mobpro.durability;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class AddActivity extends Activity implements OnClickListener {
 
+    private FloatingActionButton addButton;
     private Button scanBtn;
+    private int year_x, month_x, day_x;
+    private TextView date;
+    static final int DIALOG_ID = 0;
+
     //private TextView formatTxt;
     private TextView BarCodeNumber;
 
@@ -27,8 +40,16 @@ public class AddActivity extends Activity implements OnClickListener {
         scanBtn = (Button)findViewById(R.id.button);
         //formatTxt = (TextView)findViewById(R.id.scan_format);
         BarCodeNumber = (TextView)findViewById(R.id.scan_content);
+        date = (TextView) findViewById(R.id.date);
 
         scanBtn.setOnClickListener(this);
+
+        final Calendar cal = Calendar.getInstance();
+        year_x = cal.get(Calendar.YEAR);
+        month_x = cal.get(Calendar.MONTH);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
+
+        showDialogOnButtonClick();
     }
 
     public void onClick(View v) {
@@ -60,5 +81,39 @@ public class AddActivity extends Activity implements OnClickListener {
         }
 
     }
+
+    public void showDialogOnButtonClick() {
+        addButton = (FloatingActionButton) findViewById(R.id.addDate);
+
+        addButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(DIALOG_ID);
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_ID) {
+            return new DatePickerDialog(this, dpickerListner, year_x, month_x, day_x);
+        } else {
+            return null;
+        }
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListner = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            year_x = year;
+            month_x = monthOfYear + 1;
+            day_x = dayOfMonth;
+
+            date.setText(day_x + "." + month_x + "." + year_x);
+
+        }
+    };
 
 }
