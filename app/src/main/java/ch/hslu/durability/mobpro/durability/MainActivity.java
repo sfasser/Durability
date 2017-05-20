1dc20438b90ed82af01cca494abfd59921a4c594
sfasser <sandro.fasser@stud.hslu.ch>
2017-05-20 17:53:22 +0200

    12.05.17 21.50

--- app/src/main/java/ch/hslu/durability/mobpro/durability/MainActivity.java
+++ app/src/main/java/ch/hslu/durability/mobpro/durability/MainActivity.java
@@ -1,15 +1,23 @@
 package ch.hslu.durability.mobpro.durability;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -27,10 +35,60 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         listProducts = (ListView)findViewById(R.id.listProducts);
-        //new GetHttpResponse(this).execute();
+        //DbAdapter dbAdapter = new DbAdapter(this);
+        //dbAdapter.open();
+
+
     }
 
 
+    /*public class DbAdapter{
+        private final DBHelper dbHelper;
+        private SQLiteDatabase db;
+
+        public DbAdapter(final Context context){
+            dbHelper = new DBHelper(context);
+        }
+
+        public void open(){
+            if(db == null || !db.isOpen()){
+                db = dbHelper.getWritableDatabase();
+                try {
+                    Cursor c = db.rawQuery("SELECT date, name FROM tbl_myproducts", null);
+
+                    if (c != null ) {
+                        if  (c.moveToFirst()) {
+                            do {
+                                String firstName = c.getString(c.getColumnIndex("name"));
+                                int date = c.getInt(c.getColumnIndex("date"));
+                            }while (c.moveToNext());
+                        }
+                    }
+                } catch (SQLiteException se ) {
+                    Log.e(getClass().getSimpleName(), "Could not create or Open the database");
+                } finally {
+                    db.close();
+                }
+            }
+        }
+        public void close(){
+            dbHelper.close();
+        }
+    }
+
+    public class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(final Context context){
+            super(context, DbAdapter.DB_NAME, null, DbAdapter.DB_VERSION);
+        }
+
+        @Override
+        public void onCreate(final SQLiteDatabase db){
+            db.execSQL("CREATE TABLE tbl_myproducts (date not null, name TEXT, not null, id INTEGER PRIMARY KEY;)");
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+    }
+*/
 
 
     public void StartAddActivity(View Button) {
