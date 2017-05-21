6a002abeae4b0e3ee07a629ec105e4533301a4e7
sfasser <sandro.fasser@stud.hslu.ch>
2017-05-21 22:51:09 +0200

    12.05.17 21.50

--- app/src/main/java/ch/hslu/durability/mobpro/durability/MainActivity.java
+++ app/src/main/java/ch/hslu/durability/mobpro/durability/MainActivity.java
@@ -13,8 +13,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.sql.Connection;
 import java.sql.Date;
@@ -35,14 +39,72 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         listProducts = (ListView)findViewById(R.id.listProducts);
-        //DbAdapter dbAdapter = new DbAdapter(this);
-        //dbAdapter.open();
 
+        // TodoDatabaseHandler is a SQLiteOpenHelper class connecting to SQLite
+        DBHelper handler = new DBHelper(this);
+        // Get access to the underlying writeable database
+        SQLiteDatabase db = handler.getWritableDatabase();
+        // Query for items from the database and get a cursor back
+        Cursor todoCursor = db.rawQuery("SELECT date, name FROM tbl_myproducts", null);
+
+        // Setup cursor adapter using cursor from last step
+        TodoCursorAdapter todoAdapter = new TodoCursorAdapter(this, todoCursor);
+        // Attach cursor adapter to the ListView
+        listProducts.setAdapter(todoAdapter);
 
     }
 
 
-    /*public class DbAdapter{
+
+
+    public class DBHelper extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "MyProducts";
+        private static final int DATABASE_VERSION = 1;
+
+        public DBHelper(final Context context){
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(final SQLiteDatabase db){
+            db.execSQL("CREATE TABLE [IF NOT EXISTS] tbl_myproducts (date DATE NOT NULL, name TEXT NOT NULL, id INTEGER PRIMARY KEY;)");
+            db.execSQL("INSERT INTO tbl_myproducts (date, name) VALUES ('fred','jan 1 2009 13:22:15');");
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+    }
+
+
+
+    public class TodoCursorAdapter extends CursorAdapter {
+        public TodoCursorAdapter(Context context, Cursor cursor) {
+            super(context, cursor, 0);
+        }
+
+        // The newView method is used to inflate a new view and return it,
+        // you don't bind any data to the view at this point.
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            return LayoutInflater.from(context).inflate(R.layout.myproducts, parent, false);
+        }
+
+        // The bindView method is used to bind all data to a given view
+        // such as setting the text on a TextView.
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            // Find fields to populate in inflated template
+            TextView tvBody = (TextView) view.findViewById(R.id.tvBody);
+            TextView tvPriority = (TextView) view.findViewById(R.id.tvPriority);
+            // Extract properties from cursor
+            String body = cursor.getString(cursor.getColumnIndexOrThrow("body"));
+            int priority = cursor.getInt(cursor.getColumnIndexOrThrow("priority"));
+            // Populate fields with extracted properties
+            tvBody.setText(body);
+            tvPriority.setText(String.valueOf(priority));
+        }
+    }
+
+    /*   public class DbAdapter{
         private final DBHelper dbHelper;
         private SQLiteDatabase db;
 
@@ -74,19 +136,6 @@ public class MainActivity extends AppCompatActivity {
         public void close(){
             dbHelper.close();
         }
-    }
-
-    public class DBHelper extends SQLiteOpenHelper {
-        public DBHelper(final Context context){
-            super(context, DbAdapter.DB_NAME, null, DbAdapter.DB_VERSION);
-        }
-
-        @Override
-        public void onCreate(final SQLiteDatabase db){
-            db.execSQL("CREATE TABLE tbl_myproducts (date not null, name TEXT, not null, id INTEGER PRIMARY KEY;)");
-        }
-
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
     }
 */
 
